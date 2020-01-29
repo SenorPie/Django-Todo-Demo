0adc42e85f61bf70ec7ea62064a36cc35ee6f526
SenorPie <kaihanhakimi5@gmail.com>
2020-01-29 15:53:02 +0100

    Added new view for adding tasks

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import NewTodo
+from .models import Todo
+
 
 # Create your views here.
+def index(request):
+    tasks = Todo.objects.all()
+    context = {'tasks': tasks, 'todoform': NewTodo()}
+    return render(request=request, template_name='index.html', context=context)
+
+
+def add_todo(request):
+    task = request.POST['task']
+    todo_item = Todo(task=task)
+    todo_item.save()
+    return redirect('index')
